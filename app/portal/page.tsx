f5d393dbe0e0017356891b33062168b746ea43c7
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-02-19 13:46:26 +0800

    ✨ feat(login): add login form
    AFFECTED:
    - update color palette to adjust with ui
    - create component inputField.tsx
    
    NOTE:
    ALL DESIGNS ARE INITIAL

--- app/portal/page.tsx
+++ app/portal/page.tsx
@@ -1,9 +1,26 @@
-
+import InputField from "@/components/inputField";
 
 export default function Portal() {
     return (
-        <main>
-            <h1>Portal</h1>
+        <main className="flex justify-center items-center min-w-screen min-h-screen">
+            <div className="flex px-4 py-6 flex-col gap-6 w-2/5 bg-(--card) border border-(--border) rounded">
+                <div>
+                    <h1 className="text-(--heading) text-[1.5rem] font-bold">Login</h1>
+                    <p className="text-(--secondary) text-[1.25rem) font-light">Welcome back!</p>
+                </div>
+                <div>
+                    <form action="#" method="post">
+                        {/* EMAIL */}
+                        <InputField label="Email" type="email" placeholder="Enter your email" />
+                        {/* PASSWORD */}
+                        <InputField label="Password" type="password" placeholder="Enter your password" />
+                        {/* SUBMIT */}
+                        <button type="submit" className="w-full px-4 py-2 bg-(--primary-action) font-bold rounded cursor-pointer hover:bg-(--primary-hover)">
+                            Login
+                        </button>
+                    </form>
+                </div>
+            </div>
         </main>
     );
 }
---
+++ components/inputField.tsx
@@ -0,0 +1,18 @@
+type InputFieldProps = {
+  label: string;
+  type: string;
+  placeholder: string;
+};
+
+export default function InputField({ label, type, placeholder }: InputFieldProps) {
+  return (
+    <div className="mb-4">
+      <label className="block text-sm font-medium text-foreground/80 mb-2">{label}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="w-full px-4 py-2 border border-(--border) rounded-md focus:outline-none focus:ring focus:ring-(--secondary)"
+      />
+    </div>
+  );
+}

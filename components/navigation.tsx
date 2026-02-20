b35cc2ab6fe618b5bc9ada9ca73c7b2c41edf7d0
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-02-20 19:24:32 +0800

    💄 feat(nav): update spacing and font sizes for different screen sizes

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,20 +2,20 @@ import Button from "./button";
 
 export default function Navigation() {
   return (
-    <nav className="absolute top-0 left-0 right-0 flex justify-between items-center px-6 pt-16">
+    <nav className="absolute top-0 left-0 right-0 flex justify-between items-center px-6 pt-4 lg:pt-6 xl:pt-16">
       {/* MENU */}
       <div>
-        <button type="button" className="text-(--secondary) text-[1.5rem] font-light tracking-[0.1rem]">MENU</button>
+        <button type="button" className="text-(--secondary) text-[1rem] xl:text-[1.5rem] font-light tracking-[0.1rem] cursor-pointer">MENU</button>
       </div>
 
       {/* COMPANY LOGO */}
       <div>
-        <button type="button" className="text-foreground text-[2rem] font-light tracking-[0.5rem]">OPS</button>
+        <button type="button" className="text-foreground text-[1rem] xl:text-[2rem] font-light tracking-[0.5rem] cursor-pointer">OPS</button>
       </div>
 
       {/* CONTACT */}
       <div>
-        <button type="button" className="text-(--secondary) text-[1.5rem] font-light tracking-[0.1rem]">CONTACT ME</button>
+        <button type="button" className="text-(--secondary) text-[1rem] xl:text-[1.5rem] font-light tracking-[0.1rem] cursor-pointer">CONTACT ME</button>
       </div>
 
     </nav>

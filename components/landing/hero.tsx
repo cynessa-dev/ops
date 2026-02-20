9b0f9bea7afddc27ed2fee8c5725650af74914fc
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-02-20 19:20:37 +0800

    💄 feat(spacing): update padding in hero and nav to x 24px

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/button";
 
 export default function Hero() {
     return (
-        <section className="min-w-full min-h-screen pt-32">
+        <section className="min-w-full min-h-screen px-6 pt-32">
             <div id="header" className="flex flex-col gap-4 w-4/5 select-none">
                 <h1 className="text-(--heading) text-[4rem] md:text-[5rem] xl:text-[6rem] font-black tracking-[-0.1rem]">OPERATE WITH EASE</h1>
                 <p className="text-(--secondary) text-[2rem] leading-8 tracking-[0.1rem]">Work, collaborate, and communicate <br /> with the right tools, all in one place!</p>
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,7 +2,7 @@ import Button from "./button";
 
 export default function Navigation() {
   return (
-    <nav className="absolute top-0 left-0 right-0 flex justify-between items-center px-6">
+    <nav className="absolute top-0 left-0 right-0 flex justify-between items-center px-6 pt-16">
       {/* MENU */}
       <div>
         <button type="button" className="text-(--secondary) text-[1.5rem] font-light tracking-[0.1rem]">MENU</button>

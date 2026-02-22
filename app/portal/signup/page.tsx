de06ebdc284203f357c205213fcd85a805834d87
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-02-22 23:12:55 +0800

    ✨feat(signup): implement signup page using useSignUp hook

---
+++ app/portal/signup/page.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { useSignUp } from "@/lib/hooks/useSignUp";
+
+import InputField from "@/components/inputField";
+import Navigation from "@/components/navigation";
+
+export default function Portal() {
+    const {
+        email,
+        setEmail,
+        password,
+        setPassword,
+        confirmPassword,
+        setConfirmPassword,
+        message,
+        error,
+        submit,
+    } = useSignUp();
+
+
+    return (
+        <main className="flex justify-center items-center min-w-screen min-h-screen">
+            <Navigation />
+            <div className="flex px-4 py-6 flex-col gap-6 w-2/5 bg-(--card) border border-(--border) rounded">
+                <div>
+                    <h1 className="text-(--heading) text-[1.5rem] font-bold">Sign Up</h1>
+                    <p className="text-(--secondary) text-[1.25rem) font-light">Let's get you setup!</p>
+                </div>
+                <div>
+                    <form method="post">
+                        {/* EMAIL */}
+                        <InputField 
+                            label="Email" 
+                            type="email" 
+                            placeholder="Enter your email" 
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        {/* PASSWORD */}
+                        <InputField 
+                            label="Password" 
+                            type="password" 
+                            placeholder="Enter your password" 
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        {/* CONFIRM PASSWORD */}
+                        <InputField 
+                            label="Confirm Password" 
+                            type="password" 
+                            placeholder="Confirm your password" 
+                            value={confirmPassword} 
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+
+                        {/* DISPLAY ERROR MESSAGE */}
+                        {error && <p className="text-[--danger] text-sm mt-2">{error}</p>}
+
+                        {/* DISPLAY SUCCESS MESSAGE */}
+                        {message && <p className="text-[--success] text-sm mt-2">{message}</p>}
+                        
+                        {/* SUBMIT */}
+                        <div className="pt-4">
+                            <button className="w-full py-2 px-4 bg-(--primary-action) text-foreground font-bold rounded cursor-pointer hover:bg-(--primary-hover)" onClick={submit}>Let's get started!</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </main>
+    );
+}

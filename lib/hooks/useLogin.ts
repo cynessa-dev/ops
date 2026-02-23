d01e8f85e2d40323ce01e602b6cb3bad0752eeb8
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-02-23 09:02:12 +0800

    🎨 style(useLogin): add comments to improve clarity

--- lib/hooks/useLogin.ts
+++ lib/hooks/useLogin.ts
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 import { handleLogin } from "../services/handleLogin";
 
 export function useLogin() {
+    // User Crednetials
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    // Feedback State
     const [message, setMessage] = useState<string | null>(null);
     const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
 
     const submit = async (e: React.SyntheticEvent) => {
         e.preventDefault(); // Prevent form from refreshing the page
-        
+
         // reset any previous state before trying again
         setError(null);
         setMessage(null);

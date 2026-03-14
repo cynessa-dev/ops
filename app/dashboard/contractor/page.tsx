1173624421afcffd1dade2504627ac1ba2f49e32
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-14 20:24:04 +0800

    🚧 test: remove DeliveryTable component for testing purposes

--- app/dashboard/contractor/page.tsx
+++ app/dashboard/contractor/page.tsx
@@ -3,7 +3,7 @@ import DeliveryTable from "@/components/manager/deliveryTable";
 export default function Manager() {
     return(
         <main>
-            <DeliveryTable />
+            
         </main>
     );
 }

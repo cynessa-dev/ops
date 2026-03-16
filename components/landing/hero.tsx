61da60fa82ad8fd4229bd8efc64fb7f26e135775
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-16 20:40:01 +0800

    ⏪️ style(hero): revert hero to barebons to prepare for a new look

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -1,24 +1,7 @@
-import Image from "next/image";
-
-import Button from "@/components/button";
-
 export default function Hero() {
     return (
-        <section className="min-w-full min-h-screen px-6 pt-32">
-            <div id="header" className="flex flex-col gap-4 w-4/5 select-none">
-                <h1 className="text-(--heading) text-[4rem] md:text-[5rem] xl:text-[6rem] font-black tracking-[-0.1rem]">OPERATE WITH EASE</h1>
-                <p className="text-(--secondary) text-[2rem] leading-8 tracking-[0.1rem]">Work, collaborate, and communicate <br /> with the right tools — all in one place!</p>
-            </div>
-            <div id="actions" className="mt-16 w-1/5 flex gap-4">
-                <Button text="LET'S GET STARTED" variant="primary" size="lg" destination="/portal/signup" />
-            </div>
-            {/* SCROLL DOWN */}
-            <div className=" absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col justify-center items-center cursor-pointer animate-bounce">
-                <div className="flex items-center justify-center w-16 h-16 xl:w-20 xl:h-20 bg-(--card) rounded-full">
-                    <Image src="/icons/mouse.png" alt="Scroll Down" width={50} height={50} className="w-12 xl:16" />
-                </div>
-                <Image src="/icons/double-down.png" alt="Scroll Down Indicator" width={50} height={50} className="w-12 xl:16 animate-bounce" />
-            </div>
+        <section>
+
         </section>
     );
 }

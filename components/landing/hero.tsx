d7697fe0a58490df80c70627d240fc28beaca734
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-16 21:25:38 +0800

    ✨ feat(hero): prepare layout for future content

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
     return (
-        <section>
+        <section className="relative min-h-screen grid grid-cols-1 lg:grid-cols-2 overflow-hidden bg-background">
 
         </section>
     );

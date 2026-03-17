a465dfd71367e251b79ef9eb157583cf54f17154
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-17 19:55:48 +0800

    ✨ feat(hero): add cta and learn more button for sign up and documentation

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -10,6 +10,38 @@ export default function Hero() {
                 <p className="text-muted text-[1.25rem] font-light tracking-[0.05rem]">
                     Ops gives businesses a centralized, efficient way to record goods flowing in and out — through smart, customizable forms built for real operations.
                 </p>
+
+                {/* CTA */}
+                <div className="flex gap-8 mt-2">
+                    <button
+                        className="
+                            px-6 
+                            py-3 
+                            bg-primary-action 
+                            font-semibold 
+                            rounded-md 
+                            shadow-lg 
+                            shadow-primary-action/30 
+                            cursor-pointer 
+                            transition-color 
+                            duration-150 
+                            ease-in-out 
+                            active:translate-y-0 
+                            active:rotate-0 
+                            hover:bg-primary-hover 
+                            hover:shadow-xl 
+                            hover:-translate-y-2 
+                            hover:rotate-2"
+                    >
+                        Get Started!
+                    </button>
+                    
+                    <button
+                        className="btn-underline relative px-6 py-3 font-semibold cursor-pointer"
+                    >
+                        Learn More
+                    </button>
+                </div>
             </div>
         </section>
     );

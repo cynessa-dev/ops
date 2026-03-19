fc453ba4c3051635095d8a822e6f445ab33eee1e
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-19 20:21:36 +0800

    💄 style(setup): update ui to use generated color tokens for consistency

--- app/portal/signup/setup/page.tsx
+++ app/portal/signup/setup/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import InputField from "@/components/inputField";
 import { useSetup } from "@/lib/hooks/useSetup";
@@ -25,13 +25,13 @@ export default function Setup() {
 
     return (
         <main className="flex justify-center items-center min-w-screen min-h-screen">
-            <div className="flex px-4 py-6 flex-col gap-6 w-2/5 bg-(--card) border border-(--border) rounded">
+            <div className="flex px-4 py-6 flex-col gap-6 w-2/5 bg-card border border-border rounded">
                 <div>
-                    <h1 className="text-(--heading) text-[1.5rem] font-bold">Introduce Yourself!</h1>
-                    <p className="text-(--secondary) text-[1.0rem] font-light">Let&apos;s get to know more about you.</p>
+                    <h1 className="text-heading text-[1.5rem] font-bold">Introduce Yourself!</h1>
+                    <p className="text-secondary text-[1.0rem] font-light">Let&apos;s get to know more about you.</p>
                 </div>
                 <div>
-                    <form method="post">
+                    <form method="POST">
                         {/* FIRST NAME */}
                         <InputField 
                             label="First Name" 
@@ -50,7 +50,7 @@ export default function Setup() {
                         />
                         {/* ROLE */}
                         <select 
-                            className="w-full px-3 py-2 bg-(--card) border border-(--border) rounded mt-4 focus:outline-none focus:ring focus:ring-(--secondary)"
+                            className="w-full px-3 py-2 bg-card border border-border rounded mt-4 focus:outline-none focus:ring focus:ring-secondary"
                             value={role}
                             onChange={(e) => setRole(e.target.value)}
                         >
@@ -61,7 +61,7 @@ export default function Setup() {
 
                         {/* SUBMIT */}
                         <button 
-                            className="w-full mt-6 bg-(--primary-action) text-foreground py-2 rounded cursor-pointer hover:bg-(--primary-hover) transition-colors"
+                            className="w-full mt-6 bg-(--primary-action) text-foreground py-2 rounded cursor-pointer hover:bg-primary-hover transition-colors"
                             onClick={handleSubmit}
                         >
                             Continue

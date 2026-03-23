60ebdeb7bd846ca25238ab29f6ea81948d71e6ee
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-23 14:21:15 +0800

    🎨 style: improve structure of code in inputeField.tsx for consitency

--- components/inputField.tsx
+++ components/inputField.tsx
@@ -1,30 +1,30 @@
 type InputFieldProps = {
-  label: string;
-  type: string;
-  placeholder: string;
-  value?: string;
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+    type: string;
+    label: string;
+    placeholder: string;
+    value?: string;
+    onChange?: React.ChangeEventHandler<HTMLInputElement>;
 };
 
 export default function InputField({
-  label,
-  type,
-  placeholder,
-  value,
-  onChange,
+    label,
+    type,
+    placeholder,
+    value,
+    onChange,
 }: InputFieldProps) {
-  return (
-    <div className="mb-4">
-      <label className="block text-sm font-medium text-foreground/80 mb-2">
-        {label}
-      </label>
-      <input
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        className="w-full px-4 py-2 border border-border rounded-md focus:outline-none focus:ring focus:ring-secondary"
-      />
-    </div>
-  );
+    return (
+        <div className="mb-4">
+            <label className="block text-sm font-medium text-foreground/80 mb-2">
+                {label}
+            </label>
+            <input
+                type={type}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+                className="w-full px-4 py-2 border border-border rounded-md focus:outline-none focus:ring focus:ring-secondary"
+            />
+        </div>
+    );
 }

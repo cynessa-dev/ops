bd84bc75e31da8c8fce37a4a9381b45ca3d1a2ff
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-23 17:07:33 +0800

    🐛 fix: replace method of storing in client.ts to use cookies instead of localStorage for auth

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,10 +1,10 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 import { Database } from "@/types/supabase";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabasePublishableKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
-export const supabase = createClient<Database>(
+export const supabase = createBrowserClient<Database>(
     supabaseUrl, 
     supabasePublishableKey
 );

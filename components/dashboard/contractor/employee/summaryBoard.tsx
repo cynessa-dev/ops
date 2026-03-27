a671b5d09c5f6cee8c0246094b75b6db6c29a183
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-27 14:15:37 +0800

    ✨ feat: add statCard to summaryBoard component for statistic reporting

--- components/dashboard/contractor/employee/summaryBoard.tsx
+++ components/dashboard/contractor/employee/summaryBoard.tsx
@@ -1,10 +1,19 @@
 import Greet from "@/components/dashboard/contractor/employee/greet";
+import StatCard from "@/components/dashboard/contractor/employee/statCard"
 
 export default function SummaryBoard() {
     return (
         <div className="px-8">
             {/* GREETINGS */}
             <Greet name="Chano" />
+
+            {/* SUMMARY CARDS */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                <StatCard title="My Submissions" data="0" icon="upload_file" />
+                <StatCard title="Approved" data="0" icon="check" />
+                <StatCard title="Pending" data="0" icon="schedule" />
+                <StatCard title="This Week" data="0" icon="bar_chart" />
+            </div>
         </div>
     );
 }

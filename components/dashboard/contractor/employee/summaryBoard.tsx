943f75d3c662be76bee5a6cdeafae83c85705203
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-27 14:15:04 +0800

    ✨ feat: add summaryBoard component for greeting the user with quick data

---
+++ components/dashboard/contractor/employee/summaryBoard.tsx
@@ -0,0 +1,10 @@
+import Greet from "@/components/dashboard/contractor/employee/greet";
+
+export default function SummaryBoard() {
+    return (
+        <div className="px-8">
+            {/* GREETINGS */}
+            <Greet name="Chano" />
+        </div>
+    );
+}

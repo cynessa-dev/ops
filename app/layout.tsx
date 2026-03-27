348da5e7233c89311cfd6ca9d2a792b5d0d6058e
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-27 15:13:56 +0800

    🚨 chore: add rule to ignore no page custom font as it project clearly adheres

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
         <head>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=bar_chart,check,schedule,upload_file" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=bar_chart,check,schedule,upload_file&display=swap" />
         </head>
         <body
             className={`${nunitoSans.variable} ${nunito.variable} antialiased`}

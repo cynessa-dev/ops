cbbd8e30fe9c2dffa9417f6a3618b603dedd35e0
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-27 13:58:18 +0800

    ✨ feat: add statCard component for displaying statistics data for reporting

---
+++ components/dashboard/contractor/employee/statCard.tsx
@@ -0,0 +1,29 @@
+type CardProps = {
+    title: string;
+    data: string;
+    icon: 'bar_chart' | 'check' | 'schedule' | 'upload_file';
+}
+
+export default function StatCard({ title, data, icon }: CardProps) {
+  return (
+    <div className="flex justify-start items-center pr-6 py-4 border border-border rounded-xl not-dark:shadow-card">
+        <div className="flex px-10 w-10 h-10 shrink-0 justify-center items-center rounded-xl">
+            <span className="material-symbols-rounded">
+                { icon }
+            </span>
+        </div>
+        <div>
+            <div>
+                <h2 className="text-[1rem] font-semibold uppercase">
+                    { title }
+                </h2>
+            </div>
+            <div>
+                <h1 className="text-[1.75rem] font-bold">
+                    { data }
+                </h1>
+            </div>
+        </div>
+    </div>
+  );
+}

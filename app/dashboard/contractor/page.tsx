8e9ab11a4330367d91c3b45b049354acd1733aee
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-27 14:16:02 +0800

    ✨ feat: add summaryBoard component to contractor page

--- app/dashboard/contractor/page.tsx
+++ app/dashboard/contractor/page.tsx
@@ -1,9 +1,11 @@
 import Navigation from "@/components/navigation";
+import SummaryBoard from "@/components/dashboard/contractor/employee/summaryBoard";
 
 export default function Manager() {
     return(
         <main>
             <Navigation />
+            <SummaryBoard />
         </main>
     );
 }

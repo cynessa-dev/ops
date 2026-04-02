a16bb1c089335b08eba492018fec107e78d70329
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-04-02 20:45:31 +0800

    ✨ feat: implement new version of SummaryBoard

--- app/dashboard/contractor/employee/page.tsx
+++ app/dashboard/contractor/employee/page.tsx
@@ -7,7 +7,14 @@ export default function Manager() {
     return(
         <main>
             <Navigation />
-            <SummaryBoard />
+            <SummaryBoard 
+                statCardData={[
+                    { title: "My Submissions", data: "0", icon: "upload_file" },
+                    { title: "Approved", data: "0", icon: "check" },
+                    { title: "Pending", data: "0", icon: "schedule" },
+                    { title: "This Week", data: "0", icon: "bar_chart" }
+                ]}
+            />
             <RecordInAndOut />
             <RecentSubmissions />
         </main>

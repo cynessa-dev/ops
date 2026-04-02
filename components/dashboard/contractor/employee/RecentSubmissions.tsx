2097e052f87a7a850a1ad75f47c7803f8c3ccada
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-04-02 21:11:54 +0800

    ✨ feat: update RecentSubmissions to be reuseable across the web app

--- components/dashboard/contractor/employee/RecentSubmissions.tsx
+++ components/dashboard/contractor/employee/RecentSubmissions.tsx
@@ -1,5 +1,10 @@
 
-export default function RecentSubmissions() {
+type tableData = {
+    headers: string[];
+    rows: string[][];
+}
+
+export default function RecentSubmissions({ headers, rows }: tableData) {
     return (
         <section className="mt-12 mx-8">
             <div className="flex items-center gap-2 mb-4 text-[1.125rem] font-semibold">
@@ -10,39 +15,29 @@ export default function RecentSubmissions() {
                 <table className="w-full">
                     <thead>
                         <tr className="text-left uppercase border-b border-border">
-                            <th className="px-4 py-3 font-semibold">REF</th>
-                            <th className="px-4 py-3 font-semibold">TYPE</th>
-                            <th className="px-4 py-3 font-semibold">GOODS</th>
-                            <th className="px-4 py-3 font-semibold">QTY</th>
-                            <th className="px-4 py-3 font-semibold">DATE</th>
-                            <th className="px-4 py-3 font-semibold">STATUS</th>
+                            {
+                                headers.map((header, index) => (
+                                    <th key={index} className="px-4 py-3 font-semibold">
+                                        {header}
+                                    </th>
+                                ))
+                            }
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="border-b border-border transition-colors hover:bg-table-hover">
-                            <td className="px-4 py-3">123456</td>
-                            <td className="px-4 py-3">Purchase Order</td>
-                            <td className="px-4 py-3">Office Supplies</td>
-                            <td className="px-4 py-3">10</td>
-                            <td className="px-4 py-3">2024-06-01</td>
-                            <td className="px-4 py-3 font-bold">Approved</td>
-                        </tr>
-                        <tr className="border-b border-border transition-colors hover:bg-table-hover">
-                            <td className="px-4 py-3">123456</td>
-                            <td className="px-4 py-3">Purchase Order</td>
-                            <td className="px-4 py-3">Office Supplies</td>
-                            <td className="px-4 py-3">10</td>
-                            <td className="px-4 py-3">2024-06-01</td>
-                            <td className="px-4 py-3 font-bold">Approved</td>
-                        </tr>
-                        <tr className="border-b border-border transition-colors hover:bg-table-hover">
-                            <td className="px-4 py-3">123456</td>
-                            <td className="px-4 py-3">Purchase Order</td>
-                            <td className="px-4 py-3">Office Supplies</td>
-                            <td className="px-4 py-3">10</td>
-                            <td className="px-4 py-3">2024-06-01</td>
-                            <td className="px-4 py-3 font-bold">Approved</td>
-                        </tr>
+                        {
+                            rows.map((row, rowIndex) => (
+                                <tr key={rowIndex} className="border-b border-border transition-colors hover:bg-table-hover">
+                                    {
+                                        row.map((cell, cellIndex) => (
+                                            <td key={cellIndex} className="px-4 py-3">
+                                                {cell}
+                                            </td>
+                                        ))
+                                    }
+                                </tr>
+                            ))
+                        }
                     </tbody>
                 </table>
             </div>

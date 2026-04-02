98203ef91779f2d44e002ddaebb60ec85e8447f3
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-04-02 20:57:47 +0800

    ✨ feat: implement new icons to inbound and outbound data

--- app/dashboard/contractor/manager/page.tsx
+++ app/dashboard/contractor/manager/page.tsx
@@ -1,9 +1,18 @@
 import Navigation from "@/components/ui/Navigation";
+import SummaryBoard from "@/components/dashboard/contractor/employee/SummaryBoard";
 
 export default function Manager() {
     return(
         <main>
             <Navigation />
+            <SummaryBoard 
+                statCardData={[
+                    { title: "Pending Approval", data: "0", icon: "schedule" },
+                    { title: "Total Submissions", data: "0", icon: "check" },
+                    { title: "Inbound Today", data: "0", icon: "assignment_returned" },
+                    { title: "Outbound Today", data: "0", icon: "assignment_turned_in" }
+                ]}
+            />
         </main>
     );
 }

9d048b9e9f1f047eb3da9beda1e4dc249b996759
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-04-04 16:25:44 +0800

    ✨ feat: implement SlideOver component to slide in and out when user request

--- components/dashboard/contractor/employee/ActionCenter.tsx
+++ components/dashboard/contractor/employee/ActionCenter.tsx
@@ -2,19 +2,35 @@
 
 import Button from "@/components/ui/Button";
 
-export default function ActionCenter() {
-    function handleRecordInboundGoods() {
+type ActionCenterProps = {
+    onOpen: () => void;
+};
+
+export default function ActionCenter({ onOpen }: ActionCenterProps) {
+    function recordInboundGoods() {
         // Logic for recording inbound goods goes here
     }
 
-    function handleRecordOutboundGoods() {
+    function recordOutboundGoods() {
         // Logic for recording outbound goods goes here
+    }
+
+    function handleClick(method: 'inbound' | 'outbound') {
+        if (method === 'inbound') {
+            recordInboundGoods();
+        } else if (method === 'outbound') {
+            recordOutboundGoods();
+        } else {
+            return; // Invalid method, do nothing
+        }
+
+        onOpen(); // Open the slide over after handling the action
     }
 
     return (
         <section className="flex flex-col space-x-4 mx-8 my-4 md:flex-row">
-            <Button type="button" label="Record Inbound Goods" action={ handleRecordInboundGoods } />
-            <Button type="button" label="Record Outbound Goods" action={ handleRecordOutboundGoods } />
+            <Button type="button" label="Record Inbound Goods" action={ () => handleClick('inbound') } />
+            <Button type="button" label="Record Outbound Goods" action={ () => handleClick('outbound') } />
         </section>
     );
 }

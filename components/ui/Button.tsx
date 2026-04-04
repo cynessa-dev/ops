b9221e7df9ff907dfae0eb82c3eba4218d60e8c0
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-04-04 19:08:36 +0800

    ✨ feat: add new danger variant for dangerous buttons

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,38 +1,40 @@
 type ButtonProps = {
     type?: 'submit' | 'button';
     label: string;
+    accent?: 'primary' | 'danger';
     action?: () => void;
 };
 
 export default function Button({ 
-    type = 'button', 
-    label, 
-    action 
+    type = 'button',
+    label,
+    accent = 'primary',
+    action
 }: ButtonProps) {
 
     return (
         <button
             type={ type }
             onClick={ action }
-            className="
+            className={`
                 mt-4 
                 px-6 
                 py-3 
                 w-full 
-                bg-primary-action 
+                ${ accent === 'primary' ? 'bg-primary-action' : 'bg-danger' }
                 font-semibold 
                 rounded-md 
                 shadow-lg 
-                shadow-primary-action/30 
+                ${ accent === 'primary' ? 'shadow-primary-action/30' : 'shadow-danger/30' }
                 cursor-pointer 
                 transition-color 
                 duration-150 
                 ease-in-out 
                 active:translate-y-0 
                 active:rotate-0 
-                hover:bg-primary-hover 
+                ${ accent === 'primary' ? 'hover:bg-primary-hover' : 'hover:bg-danger-hover' }
                 hover:shadow-xl
-            "
+            `}
         >
             { label }
         </button>
